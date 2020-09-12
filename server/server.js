feeb182547a39f2b23978be933f5a6fad7f80e11
Josh Rogen <rogenjh@gmail.com>
2020-09-12 14:56:29 -0400

    added integration with strava api

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const axios = require('axios')
+require('dotenv').config()
 const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/calendar', (req, res) => {
+  axios.get('https://www.strava.com/api/v3/athlete/activities', {
+    headers: {
+      'Authorization': process.env.STRAVA_TOKEN
+    }
+  })
+  .then((response) => {
+    console.log(response.data);
+    res.send(response.data)
+  })
+  .catch((error) => {
+    res.sendStatus(500)
+  })
 })
 
 app.listen(port, () => {
